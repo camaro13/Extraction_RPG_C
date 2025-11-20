716ce354081a73c5a44f0002fbb95f2b5bd93db2
seok1379 <yhoh1005@naver.com>
2025-11-20 20:18:48 +0900

    Create lllustratedguide.c

---
+++ lllustratedguide.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int attackInput;
+
+    printf("------------------------------------------\n");
+    printf("                 공격 설명                \n\n");
+
+    printf("- 방어형 숫자\n");
+    for (int i = 1; i <= 4; i++)
+        printf("%d\t", i);
+    printf("\n\n");
+
+    printf("- 공격형 숫자\n");
+    for (int i = 5; i <= 8; i++)
+        printf("%d\t", i);
+    printf("\n\n");
+
+    printf("9 (유니크 아이템 전용)\n");
+
+    printf("------------------------------------------\n\n");
+
+    printf("공격 입력 테스트\n");
+    printf("입력 : ");
+    scanf_s("%d", &attackInput);
+
+    printf("------------------------------------------\n");
+
+    printf("입력한 공격 숫자: %d\n", attackInput);
+
+    return 0;
+}
